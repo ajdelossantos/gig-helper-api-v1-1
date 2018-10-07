61314b6b4b3fab07264818d04ce40cd81ad5ab98
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2018-10-07 15:27:22 -0700

    Test adding users successfully

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
+const catchErrors = require('../errorHandlers');
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+UserSchema.statics.generatePasswordHash = function generatePasswordHash(
+  newUser
+) {
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, async (err, hash) => {
+      try {
+        newUser.password = hash;
+        const user = await newUser.save();
+        return user;
+      } catch (err) {
+        throw err;
+      }
+    });
+  });
+};
+
+module.exports = User = mongoose.model('users', UserSchema);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../../models/User');
+
 router.get('/test', (req, res) => {
   res.json({
     message: 'Users route'
   });
+});
+
+router.post('/register', async (req, res) => {
+  const user = await User.findOne({
+    email: req.body.email
+  });
+
+  if (user) {
+    return res
+      .status(400)
+      .json({ email: 'A user has already registered with this address' });
+  } else {
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    });
+
+    User.generatePasswordHash(newUser);
+    return res.json(newUser);
+  }
 });
 
 module.exports = router;
