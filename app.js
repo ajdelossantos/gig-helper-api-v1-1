aa67d46877e9e90ebadb888395e69c25e8f23b8f
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2018-10-07 14:22:55 -0700

    Implement users and events routes; setup body-parser middlewares

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+// Instantiates an Express instance and allows app methods
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
+
+// MongoDB and Mongoose
+const mongoose = require('mongoose');
 const db = require('./config/secrets').mongoURI;
 
 mongoose
@@ -8,10 +11,26 @@ mongoose
   .then(() => console.log('connected to MongoDB successfully!'))
   .catch(err => console.log(err));
 
+// Allows parsing of JSON sent to frontend
+const bodyParser = require('body-parser');
+
+// body-parser middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes
+const users = require('./routes/api/users');
+const events = require('./routes/api/events');
+
+app.use('/api/users', users);
+app.use('/api/events', events);
+
+// Temporary index page
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello world!' });
+  res.send('<h2>Hello world!</h2>');
 });
 
+// Web Server
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
