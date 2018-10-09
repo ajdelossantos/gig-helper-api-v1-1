aefbb9e5e2fd757bfe6018c939fb4b8355fb505f
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2018-10-08 21:43:55 -0700

    Refactor bcrypt methods onto session controller

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -1,14 +1,20 @@
 const jsonwebtoken = require('jsonwebtoken');
 const keys = require('../config/secrets');
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/User');
 
 exports.registerUser = async (req, res) => {
+  // const { errors, isValid } = validateRegisterInput(req.body);
+
+  // if (!isValid) {
+  //   return res.status(400).json(errors);
+  // }
+
   const user = await User.findOne({ email: req.body.email });
 
   if (user) {
-    return res
-      .status(400)
-      .json({ email: 'A user has already registered with this address' });
+    return res.status(400).json({ email: 'User already exists' });
   } else {
     const newUser = new User({
       name: req.body.name,
@@ -16,35 +22,68 @@ exports.registerUser = async (req, res) => {
       password: req.body.password
     });
 
-    User.generatePasswordHash(newUser);
-    return res.json(newUser);
+    bcrypt.hash(newUser.password, 10, (err, hash) => {
+      if (err) throw err;
+      newUser.password = hash;
+      newUser
+        .save()
+        .then(user => {
+          const payload = { id: user.id, name: user.name };
+
+          jsonwebtoken.sign(
+            payload,
+            keys.secretOrKey,
+            { expiresIn: 3600 },
+            (err, token) => {
+              if (err) throw err;
+              res.json({
+                success: true,
+                token: `Bearer ${token}`
+              });
+            }
+          );
+        })
+        .catch(err => console.log(err));
+    });
   }
 };
 
 exports.loginUser = async (req, res) => {
+  // const { errors, isValid } = validateLoginInput(req.body);
+
+  // if (!isValid) {
+  //   return res.status(400).json(errors);
+  // }
+
   const email = req.body.email;
   const password = req.body.password;
 
   const user = await User.findOne({ email });
   if (!user) {
-    return res.status(404).json({ email: 'This user does not exist' });
+    return res.status(400).json({ email: 'This user does not exist' });
   }
 
-  const isMatch = await User.comparePasswords(password, user.password);
+  bcrypt
+    .compare(password, user.password)
+    .then(isMatch => {
+      if (isMatch) {
+        const payload = { id: user.id, name: user.name };
 
-  if (isMatch) {
-    const payload = { id: user.id, name: user.name };
-
-    jsonwebtoken.sign(
-      payload,
-      keys.secretOrKey,
-      // key expires in one hour
-      { expiresIn: 3600 },
-      (err, token) => {
-        return res.json({ success: true, token: `Bearer ${token}` });
+        jsonwebtoken.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            if (err) throw err;
+            res.json({
+              success: true,
+              token: `Bearer ${token}`
+            });
+          }
+        );
+      } else {
+        return res.status(400).json({ password: 'Incorrect password' });
       }
-    );
-  } else {
-    res.status(400).json({ password: 'Incorrect password' });
-  }
+    })
+    .catch(err => console.log(err));
 };
--- models/User.js
+++ models/User.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const bcrypt = require('bcryptjs');
 
 const UserSchema = new Schema({
   name: {
@@ -20,26 +19,5 @@ const UserSchema = new Schema({
     default: Date.now
   }
 });
-
-UserSchema.statics.generatePasswordHash = function generatePasswordHash(
-  newUser
-) {
-  bcrypt.hash(newUser.password, 10, async (err, hash) => {
-    if (err) {
-      throw err;
-    } else {
-      newUser.password = hash;
-      const user = await newUser.save();
-      return user;
-    }
-  });
-};
-
-UserSchema.statics.comparePasswords = async function comparePasswords(
-  inputPassword,
-  userPassword
-) {
-  return await bcrypt.compare(inputPassword, userPassword);
-};
 
 module.exports = User = mongoose.model('users', UserSchema);
